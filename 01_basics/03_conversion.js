9e5a677b3f4fb1d16a646d36e03588b58b73f59e
Renish savaliya <renishsavaliya66@gmail.com>
2026-01-28 12:47:03 +0530

    conversion Operation

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -21,6 +21,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "renish" => true
 
 let someNumber = 33
+
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ******************* Operatons ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "renish"
+
+let str3 = str1 + str2
+console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log((3 + 4) * 5 % 3);
+
+// console.log(true++);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
