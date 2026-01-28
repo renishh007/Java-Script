d0ed4feeef87128eb3d9f0e737d0286b609cd308
Renish savaliya <renishsavaliya66@gmail.com>
2026-01-28 11:45:22 +0530

    conversion Operation

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,26 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score)); 
+
+let  valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "renish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "renish" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
