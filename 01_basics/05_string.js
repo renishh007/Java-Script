238d57875c83ced616b077a1d575bb35f6595a72
Renish savaliya <renishsavaliya66@gmail.com>
2026-01-28 16:31:10 +0530

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,38 @@
+const name = "renish"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+
+console.log(`Hello my name is ${name} and my repo count is $ {repoCount}`);
+
+const gameName = new String('renish-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 7)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 7)
+console.log(anotherString);
+
+
+const newStringOne = "  renish"
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://www.instagram.com/renishh_07/#"
+
+console.log(useLayoutEffect.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
