4c3ce51388c4c477ac321c2d6834e26345ee402f
Renish savaliya <renishsavaliya66@gmail.com>
2026-01-30 12:36:28 +0530

    function with objects & array in js

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -25,3 +25,36 @@ function loginUserMessage(username = "sam"){
     }
     return `${username} just logged in`
 }
+
+// console.log(loginUserMessage("renish"))
+// console.log(loginUserMessage("renish"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "renish",
+    parice: "200"
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 600, 1000]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 600, 1000]));
