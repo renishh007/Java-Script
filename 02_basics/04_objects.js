6024892ef2e67351aa38fdd5ba808a5e9891ac11
Renish savaliya <renishsavaliya66@gmail.com>
2026-01-30 12:32:33 +0530

    object by user

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -54,10 +54,35 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "1000",
+    courseInstructor: "renish"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "renish",
+//     "coursename": "ja in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
